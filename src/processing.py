e52c8354623a1aad63d2acf3f748f2cee8cdae38
Alex_Pavlov <alexey.s.pavlov@mail.ru>
2025-08-17 20:40:31 +0300

    function fix

---
+++ src/processing.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+
+def filter_by_state(list_not_filtered: list, state: str = "EXECUTED") -> list:
+    """
+    Функция которая фильтрует списки
+    словарей по нужному параметру
+    """
+    list_filtered = []
+    for dict_item in list_not_filtered:
+        for item in dict_item.values():
+            if item == state:
+                list_filtered.append(dict_item)
+    return list_filtered
+
+
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция которая принимает список словарей
+    и необязательный параметр, задающий порядок сортировки.
+    Функция должны возвращать новый список отсортированный по дате.
+    """
+    return sorted(
+        data,
+        key=lambda item: datetime.strptime(item["date"], "%Y-%m-%dT%H:%M:%S.%f"),
+        reverse=reverse,
+    )
