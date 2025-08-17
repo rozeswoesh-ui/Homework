a590edcad4526cce6f8840698a7bc28ddfa1f1ce
Alex_Pavlov <alexey.s.pavlov@mail.ru>
2025-08-17 19:10:31 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(account_info: str) -> str:
     """Функция обработки информации о картах и о счетах"""
@@ -7,12 +8,12 @@ def mask_account_card(account_info: str) -> str:
         parts = account_info.split()
         return f"Счет {get_mask_account(parts[-1])}"
     else:
-        parts = account_info.rsplit(' ', 1)  # Разделяем по последнему пробелу
+        parts = account_info.rsplit(" ", 1)  # Разделяем по последнему пробелу
         return f"{parts[0]} {get_mask_card_number(parts[1])}"
 
 
 def get_date(date_str: str) -> str:
     """Преобразует дату из формата '2024-03-11T02:26:18.671407' в '11.03.2024'"""
     date_part = date_str[:10]  # "2024-03-11"
-    year, month, day = date_part.split('-')
+    year, month, day = date_part.split("-")
     return f"{day}.{month}.{year}"
